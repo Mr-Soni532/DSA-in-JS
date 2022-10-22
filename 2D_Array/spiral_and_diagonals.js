41b3ddb4194c0c75eccefaa27f9892ff789ae9d2
Bhupender Soni <official.soni532@gmail.com>
2022-10-22 12:38:56 +0530

    longest unique string

--- 2D_Array/spiral_and_diagonals.js
+++ 2D_Array/spiral_and_diagonals.js
@@ -1,27 +1,27 @@
 //Given a spiral traversed arr in which we need to get the sum of diagonals imaginig the original matrix.
-
-//approach -> in the spiral travesed array the diagonal elements are placed at a certain distance and there is pattern of loop in that.. 
-// Starting with the finding the gap in the 0th row by doing -2 sustituting the corner elements.. 
+//approach -> in the spiraly travesed array the diagonal elements are placed at a certain distance and there is pattern of loop in that.. 
+// Starting with finding the gap in the 0th row by sustituting 2 from the total elements in the row.. 
 // Then run a loop for 5 time only for first time;
-// keep the index from 0 and update in the loop by (gap + 1) by which we can directly jump to the upcomming diagonla elements.
+// Then run a loop for 4 time only for rest of the iteration;
+// keep the index from 0 and update in the loop by (gap + 1) by which we can directly jump to the upcomming diagonla element.
 // after every cycle (end of loop) we update gap by subtratng -2 from it to get in the inner row for every iteration.  
-function  countElement(size, arr){
-    let n = size; 
+function countElement(n, arr) {
     let sum = 0;
     let gap = n;
-    let index = 0; 
+    let index = 0;
     let flag = true; // Handle the while loop 
     let indexFlag = true; // Handle the loop count and first element without updating index value;
-  while(flag){
-         gap = gap - 2;
+    while (flag) {                                          // 9 -> 3  gap = 3-2 = 1 
+        gap = gap - 2; // -1
         if (gap < 0) gap = 0;
         if (gap < 2) flag = false;
 
         let i = indexFlag ? 5 : 4;
         if (n == 1) i = 1;
 
-        for (let j = 0; j < i; j++) {
+        for (let j = 0; j < i; j++) { // O(1)
             if (arr[index] == undefined) break; // get out of scop 
+
             if (indexFlag) {  // runs only for 1st element
                 sum += arr[index];
                 indexFlag = false;
@@ -32,6 +32,7 @@ function  countElement(size, arr){
                 sum += arr[index];
             }
         }
-   }
+    }
     console.log(sum)
 }
+
---
+++ slidingWindow && 2pointer/longest_unique_string.js
@@ -0,0 +1,17 @@
+function cal(str){
+    let obj = {};
+    let max_length = 0;
+    let start = 0;
+    for(i in str){
+        if(str[i] in obj){
+            //shift start to the next duplicate value;
+            // using max to prevent start to go back from the current position 
+            // it should always go forward.
+            start = Math.max(start, obj[str[i]] + 1);
+        }
+        obj[str[i]] = i; //updating obj
+        max_length = Math.max(max_length, i-start+1); 
+        // max length is basically the length of the window which is start -> i
+    }
+    console.log(max_length);
+}
