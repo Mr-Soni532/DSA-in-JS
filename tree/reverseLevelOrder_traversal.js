a7a92d9a069f1a241e4a0d1eb9aacb92b25bb32e
Bhupender Soni <official.soni532@gmail.com>
2023-03-08 19:13:59 +0530

    reverse_levelOrderTraversal

---
+++ tree/reverseLevelOrder_traversal.js
@@ -0,0 +1,35 @@
+
+// Definition of Binary Tree Node
+// class Node {
+//     constructor(val) {
+//         this.val = val;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
+
+function levelOrderTraversal(root){
+    let ans = [[]];
+    let index = 0;
+    let queue = [];
+    queue.push(root);
+    queue.push(null);
+        while(queue.length){
+            let temp = queue.shift();
+            
+            if(temp == null){
+                if(queue.length){
+                queue.push(null);
+                ans.push([]);
+                index++;
+                }
+            }else {
+                ans[index].push(temp.val);
+                
+                if(temp.left != null) queue.push(temp.left)
+                
+                if(temp.right != null) queue.push(temp.right)
+            }
+        }
+        return ans.reverse();     
+}
